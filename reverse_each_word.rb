f693bb042531bdc15e60dad1e4717100ecd06ca5
Valentino Wong <valentino.wong@gmail.com>
2019-07-01 21:36:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 # def reverse_each_word(string)
 #   arr = string.split
-#  arr_reversed = []
-#  arr.each do |n|
-#    arr_reversed << n.reverse
-#  end
-#  p arr_reversed.join(" ")
+#   arr_reversed = []
+#   arr.each do |n|
+#     arr_reversed << n.reverse
+#   end
+#   p arr_reversed.join(" ")
 # end
 
 def reverse_each_word(string)
