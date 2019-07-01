0b4bdfea98efbfb3c12a7f4c8ff6db8fc720d318
Valentino Wong <valentino.wong@gmail.com>
2019-07-01 21:35:09 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+# def reverse_each_word(string)
+#   arr = string.split
+#  arr_reversed = []
+#  arr.each do |n|
+#    arr_reversed << n.reverse
+#  end
+#  p arr_reversed.join(" ")
+# end
+
+def reverse_each_word(string)
+  arr = string.split
+  reversed = arr.collect do |n| n.reverse
+  end
+  p reversed.join(" ")
+end
